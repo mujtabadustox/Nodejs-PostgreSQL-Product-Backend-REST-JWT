e39795237876f41e6cdf073e9a49a35323facfa0
mujtabadustox <i190577@nu.edu.pk>
2023-06-24 00:31:26 +0500

    APIs Done

--- models/orderModel.js
+++ models/orderModel.js
@@ -28,70 +28,79 @@ const AddOrder = async (body) => {
   console.log("abay", body);
   const content = body.products;
 
+  const OrderData = await knex.raw(`SELECT * FROM "order";`);
+
   console.log("yar", content);
   let sum = 0;
+  let id = 0;
+
   for (let i = 0; i < content.length; i++) {
-    let qty = content[i].quantity;
-
-    console.log("QUAN", qty);
-
     const data = await knex.raw(`SELECT "quantity" , "price" FROM "product"
-        WHERE "product_id" = ${content[i].product_id};`);
+    WHERE "product_id" = ${content[i].product_id};`);
 
-    let price = data.rows[0].price;
-    console.log("prod", i, "price", price);
-    let newPrice = qty * price;
-    console.log("prod", i, "newprice", newPrice);
+    let qty = content[i].quantity;
+    let newQty = data.rows[0].quantity - qty;
+    console.log("item ki qty", data.rows[0].quantity);
+    console.log("new qty", newQty);
 
-    sum += newPrice;
-    console.log("sum", sum);
+    if (i === 0) {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = (now.getMonth() + 1).toString().padStart(2, "0");
+      const day = now.getDate().toString().padStart(2, "0");
+      const hours = now.getHours().toString().padStart(2, "0");
+      const minutes = now.getMinutes().toString().padStart(2, "0");
+      const seconds = now.getSeconds().toString().padStart(2, "0");
+      const milliseconds = now.getMilliseconds().toString().padStart(6, "0");
 
-    let orderLen = await knex.raw(`SELECT * FROM "order_item";`);
+      const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
 
-    console.log("aaaaaaa", orderLen);
+      let newObj = {
+        order_id: OrderData.rows.length + 1,
+        customer_id: body.customer_id,
+        grand_total: sum,
+        created_at: timestamp,
+      };
 
-    let id = orderLen.rows.length + 1;
+      console.log("zzzzzzz", newObj);
 
-    console.log("new It id", id);
+      let ordersNew = await knex.raw(
+        `INSERT INTO "order" VALUES(${newObj.order_id}, ${newObj.customer_id}, ${newObj.grand_total}, '${newObj.created_at}');`
+      );
+    }
 
-    const OrderData = await knex.raw(`SELECT * FROM "order";`);
-
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, "0");
-    const day = now.getDate().toString().padStart(2, "0");
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
-    const seconds = now.getSeconds().toString().padStart(2, "0");
-    const milliseconds = now.getMilliseconds().toString().padStart(6, "0");
-
-    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-
-    let newObj = {
-      order_id: OrderData.rows.length + 1,
-      customer_id: body.customer_id,
-      grand_total: sum,
-      created_at: timestamp,
-    };
-
-    console.log("zzzzzzz", newObj);
-
-    let ordersNew = await knex.raw(
-      `INSERT INTO "order" VALUES(${newObj.order_id}, ${newObj.customer_id}, ${newObj.grand_total}, '${newObj.created_at}');`
-    );
-
-    let orderItem = await knex.raw(
-      `INSERT INTO "order_item" VALUES(${id}, ${newObj.order_id}, ${content[i].product_id}, ${content[i].quantity}, ${price}, ${newPrice});`
-    );
-
-    let newQty = data.rows[0].quantity - content[i].quantity;
-
-    const updateData = await knex.raw(
-      `UPDATE "product"
+    if (newQty >= 0) {
+      const updateData = await knex.raw(
+        `UPDATE "product"
               SET "quantity" = ${newQty}
               WHERE "product_id" = ${content[i].product_id};
               `
-    );
+      );
+
+      console.log("QUAN", qty);
+
+      let price = data.rows[0].price;
+      console.log("prod", i, "price", price);
+      let newPrice = qty * price;
+      console.log("prod", i, "newprice", newPrice);
+
+      sum += newPrice;
+      console.log("sum", sum);
+
+      let orderLen = await knex.raw(`SELECT * FROM "order_item";`);
+
+      console.log("aaaaaaa", orderLen);
+
+      id = orderLen.rows.length + 1;
+
+      console.log("new It id", id);
+
+      const orderId = OrderData.rows.length + 1;
+
+      let orderItem = await knex.raw(
+        `INSERT INTO "order_item" VALUES(${id}, ${orderId}, ${content[i].product_id}, ${content[i].quantity}, ${price}, ${newPrice});`
+      );
+    }
   }
 };
 
